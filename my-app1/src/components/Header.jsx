79449b04d19d21f82a38d1672a29eae1e64e50a2
NekoMi <slayraiz@gmail.com>
2019-03-07 17:38:28 +0700

    new app

---
+++ my-app1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://steamuserimages-a.akamaihd.net/ugc/966474717666916214/BDC401368964485DBB6FE35222B0E1E34D504D4F/' alt="logo" />
+    </header>
+    );
+}
+
+export default Header;
---
+++ my-app1/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src="https://bigmemes.funnyjunk.com/thumbnails/comments/I+love+costume+swap+fan+art+also+does+anyone+know+_05237975413996d621b80afc9dac6cda.png" />
+        </div>
+        <div>
+          <img src="https://steamuserimages-a.akamaihd.net/ugc/972104754098756379/449AB80F0762EB5DFFFAF5E9AC7BC3E6E64016B9/" />
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post
+          </div>
+            <div>
+              post
+          </div>
+            <div>
+              post
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
